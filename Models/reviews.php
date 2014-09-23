2f4f10119194bd3ee6c8efda927492fbe12a1d81
MohabGhanim <mohab.ghanim@gmail.com>
2014-09-23 20:11:53 +0700

    Adding function to reviews class

--- Models/reviews.php
+++ Models/reviews.php
@@ -7,10 +7,48 @@ class reviews extends base {
 		parent::__construct();
 	}
 
-	function add(){}
+	function add($user_id, $product_id, $rate, $comment){
+		$query = "INSERT INTO reviews (user_id, product_id, rate, comment) 
+					VALUES (?, ?, ?, ?)";
+		$stmt = $this->pdo->prepare($query);
+		$is_successful = $stmt->execute(array($user_id, $product_id, $rate, $comment));
+		return $is_successful; 
+	}
 
-	function delete(){}
+	function delete($user_id , $product_id){
+		$query = "DELETE FROM reviews WHERE user_id = ? AND product_id = ?";
+		$stmt = $this->pdo->prepare($query);
+		$is_successful = $stmt->execute(array($user_id, $product_id));
+		return $is_successful;
+	}
 
-	function get_reviews_by(){}
-} 
+	function get_reviews_by($params){
+		$reviews = array();
+		$query = "SELECT * FROM reviews WHERE $params";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute();
+		while($review = $stmt->fetch(PDO::FETCH_OBJ)){
+			$reviews[] = $review;
+		}
+		return $reviews;
+	}
+}
+$reviews = new reviews();
+/* Adding review */
+// $user_id = 1;
+// $product_id = 4;
+// $rate = 5;
+// $comment = "dolor sit amet, coomment";
+// var_dump($reviews->add($user_id, $product_id, $rate, $comment));
+
+/* Deleting review */
+// $user_id = 19;
+// $product_id = 2;
+// var_dump($reviews->delete($user_id, $product_id));
+
+// 
+
+// /* Getting Reviews */
+$query = "product_id = 1";
+var_dump($reviews->get_reviews_by($query));
 ?>
--- eshop.sql
+++ eshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2014 at 04:09 PM
+-- Generation Time: Sep 23, 2014 at 03:10 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -67,7 +67,19 @@ CREATE TABLE IF NOT EXISTS `products` (
   `category` varchar(50) NOT NULL,
   `added_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `description`, `price`, `discount`, `stock`, `average_rating`, `image_link`, `category`, `added_time`) VALUES
+(1, 'shaj', '', 12, 0, 0, 0, '', '', '2014-09-22 14:23:04'),
+(2, 'grj', '', 134, 0, 0, 0, '', '', '2014-09-22 14:23:15'),
+(3, 'tfr', '', 443, 0, 0, 0, '', '', '2014-09-22 14:23:25'),
+(4, 'product A', '', 10, 0, 1, 0, '', 'A', '2014-09-23 12:34:12'),
+(5, 'product B', '', 10, 0, 1, 0, '', 'A', '2014-09-23 12:34:18'),
+(6, 'product ', '', 10, 0, 1, 0, '', 'A', '2014-09-23 12:34:21');
 
 -- --------------------------------------------------------
 
@@ -83,9 +95,22 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `comment` text NOT NULL,
   `time_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`user_id`,`product_id`),
   KEY `user_id` (`user_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `user_id`, `product_id`, `rate`, `comment`, `time_added`) VALUES
+(1, 1, 1, 5, 'Very Good', '2014-09-23 12:35:20'),
+(3, 2, 1, 1, 'Bad!', '2014-09-23 12:35:45'),
+(7, 19, 1, 0, 'no comment', '2014-09-23 12:39:15'),
+(16, 1, 2, 2, 'comment', '2014-09-23 13:03:23'),
+(18, 1, 3, 4, 'Lorem ipsum dolor sit amet, coomment', '2014-09-23 13:03:58'),
+(19, 1, 4, 5, 'dolor sit amet, coomment', '2014-09-23 13:04:26');
 
 -- --------------------------------------------------------
 
@@ -101,17 +126,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`first_name`, `last_name`, `password`, `user_name`, `id`) VALUES
-('userr', 'one', '123', 'userrone', 1),
-('ufedrr', 'sofne', '123', 'sfuserrofne', 2),
+('namoo', 'naamoo', 'qaaaaz', 'naamoo', 1),
+('noaa', 'noaaa', '123', 'noaa', 2),
 ('der', 'teri', '145', 'iserteri', 3),
-('gar', 'ghti', '1dd45', 'rin', 4);
+('gar', 'ghti', '1dd45', 'rin', 4),
+('', '', '', '', 5),
+('ha', 'ho', '123', 'he', 18),
+('nemo', 'keno', '123', 'usershemo', 19),
+('newo', 'keeno', '123', 'usereshemo', 21),
+('with password', 'keenopassword', '111123', 'usemo', 22),
+('with password', 'keenopassword', '111123', 'use2mo', 26),
+('withs string', 'string', '111123', 'string passw', 28),
+('withs string', 'string', 'aaaa', 'striddng passw', 30),
+('', 'string', 'aaaa', 'strpassw', 31);
 
 --
 -- Constraints for dumped tables
