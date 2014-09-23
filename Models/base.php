d91424cb1a2b70f2a300a20d5c02f55959a7e1e1
MohabGhanim <mohab.ghanim@gmail.com>
2014-09-23 14:15:02 +0700

    Adding functions to users model

--- Models/base.php
+++ Models/base.php
@@ -9,6 +9,8 @@ class base {
 	public function __construct(){
 		try {
 			$this->pdo = new PDO("mysql:dbname=$this->dbname;host=$this->host", $this->user, $this->password);
+			/* Uncomment to view database error messages */
+		  	// $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
 			echo "PDO Error::" . $e->getMessage();
 		}
--- Models/users.php
+++ Models/users.php
@@ -7,20 +7,101 @@ class users extends base {
 		parent::__construct();
 	}
 
-	function add(){}
+	function add($first_name, $last_name, $user_name, $password){
+		$query = "INSERT INTO users (first_name, last_name, password, user_name) VALUES (?, ?, ?, ?)";
+		$stmt = $this->pdo->prepare($query);
+		$is_successful = $stmt->execute(array($first_name, $last_name, $password, $user_name));
+		return $is_successful;
+	}
 
-	function verify(){}
+	function verify($params){
+		$query = "SELECT * FROM users where $params";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute();
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		if ($result) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
-	function edit_info(){}
+	function edit_info($user_id, $edits){
+		$query = "UPDATE users set ";
+		$edits_count = count($edits);
+		foreach($edits as $column => $value){
+			$query .= "$column = '$value' ";
+			if (--$edits_count){
+				$query .= " , ";
+			}
+		}
+		$query .= "where id = ?";
+		echo $query;
+		$stmt = $this->pdo->prepare($query);
+		$is_successful = $stmt->execute(array($user_id));
+		return $is_successful;
+	}
 
-	function edit_password(){}
+	function edit_password($user_id, $old_password, $new_password){
+		$query = "SELECT * FROM users where id = ? and password = ?";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute(array($user_id, $old_password));
+		$password_match = $stmt->fetch();
+		if (!$password_match){
+			return false;
+		}else {
+			$query = "UPDATE users set password = ? where id = ?";
+			$stmt = $this->pdo->prepare($query);
+			$is_successful = $stmt->execute(array($new_password, $user_id));
+			return $is_successful? $is_successful : die("Database Error");
+		}
+	}
 
-	function get_info(){}
-	
-// Performing query::
-// $result = $this->pdo->query('SELECT * FROM users');
-// while($user = $result->fetch(PDO::FETCH_OBJ)){
-// 	$this->all[] = $user;
-// }
+	function get_info($user_id, $columns){
+		$query = "SELECT $columns from users where id = ?";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute(array($user_id));
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $result;
+	}
 }
+
+/* Tests */
+// $users = new users();
+
+/* Verifying User */
+// echo $users->verify("user_name = 'userrone' and password = 23");
+
+/* Adding new User */
+// $query_result = $users->add("withs string", "string", "striddng passw", "aaaa");
+// if ($query_result) {
+// 	echo "The result was true";
+// }else {
+// 	echo "The result was false";
+// }
+
+/* Adding new User with empty fields */
+// $query_result = $users->add("", "string", "strpassw", "aaaa");
+// if ($query_result) {
+// 	echo "The result was true";
+// }else {
+// 	echo "The result was false";
+// }
+
+/* Editing password */
+// $user_id = 1;
+// $old_password = 'qqqwer';
+// $new_password = 'qaaaaz';
+// $result = $users->edit_password($user_id, $old_password, $new_password);
+// var_dump($result);
+
+/* Selecting values */
+// $user_id = 1;
+// $columns = "first_name, last_name, id";
+// var_dump($users->get_info($user_id, $columns));
+
+/* Editing attributes */
+// $user_id = 1;
+// $edits = array("first_name"=> "namoo", "last_name"=> "naamoo", "user_name"=> "naamoo");
+// var_dump( $users->edit_info($user_id, $edits) );
 ?>
