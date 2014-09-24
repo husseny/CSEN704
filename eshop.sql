5833fdd21d75bdd61c85ee1c31c4f41194e2b742
MohabGhanim <mohab.ghanim@gmail.com>
2014-09-24 21:06:37 +0700

    Resetting database user and password

--- eshop.sql
+++ eshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2014 at 02:38 PM
+-- Generation Time: Sep 24, 2014 at 04:04 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
