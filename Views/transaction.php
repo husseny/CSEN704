c4f2d71ddd75ae46f0e3291beb47a8cd79fea8de
husseny <abdullrahman.elhusseni@gmail.com>
2014-09-26 18:10:42 +0200

    invoice page

--- Views/transaction.php
+++ Views/transaction.php
@@ -3,8 +3,17 @@
 $root = $_SERVER['DOCUMENT_ROOT'];
 include_once("$root/eshop/Views/__head.php");
 include_once("$root/eshop/Controllers/cart_controller.php");
+if(!isset($_SESSION['trans']))
+{
+	$new_path = "/eshop/Views/index.php";
+	echo "<script> location.replace('$new_path'); </script>";
+	die();
+}
 $result = $_SESSION['trans'];
+unset($_SESSION['trans']);
 $cart = $result['cart'];
+$time = date('H:i');
+$date = date('d/m/y');
 $products = $result['products'];
 $trans =1;
 ?>
@@ -33,7 +42,7 @@ foreach ($products as $product) {
 </tr>
 <tr>
     <td class="col-md-6"><b>Transaction time</b></td>
-    <td class="col-md-4 center_element bold"><b><?php var_dump($cart->transaction_time); ?></b></td>
+    <td class="col-md-4 center_element bold"><b><?php echo $time; echo " "; echo $date; ?></b></td>
 </tr>
 </table>
 </div>
